48bdcf8833f5452f1ee489652d992d0db6ad2292
Mauro Facundo Calvo Jokmanovich <132304565+MFCJ@users.noreply.github.com>
2026-06-03 19:38:27 -0300

    ejecicio-panaderia-POO

---
+++ panaderria.py
@@ -0,0 +1,33 @@
+class Panaderia:
+    def __init__ (self, nombre, precio, tipo):
+        self.__nombre = nombre
+        self.__precio = precio
+        self.__tipo = tipo
+@property
+def nombre(self):
+    return nombre
+@property
+def precio(self):
+    return precio
+@property
+def tipo(self):
+    return tipo
+
+@nombre.setter
+def nombre(self, nuevo_nombre):
+    if len(nuevo_nombre) < 0:
+        raise ValueError ("Error")
+    self.__nombre = nuevo_nombre
+@precio.setter
+def precio (self, nuevo_precio):
+    if nuevo_precio < 0:
+        raise ValueError ("Error")
+    self.__precio = nuevo_precio
+@tipo.setter
+def tipo (self, valor):
+    if valor not in ("unidad", "peso"):
+        raise ValueError ("Error de precio")
+    self.__tipo = valor
+
+def __str__ (self):
+    return f"{self.__nombre}, - ${self.__precio} ({self.__tipo})"
